bdce1eaec11b06c5be81ff5cdfae1c7c3a2e3c17
Vitaliy Sergeychik <veesergey@gmail.com>
2017-10-23 12:13:04 -0400

    initial push

---
+++ app/src/main/java/com/example/vsergeychik390/hccmaps/DestinationPage.java
@@ -0,0 +1,17 @@
+package com.example.vsergeychik390.hccmaps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class DestinationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination_page);
+    }
+
+
+}
