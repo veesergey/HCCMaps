6f4f0911403fc8ba33af18d6db316fa02a6fd931
djordjev7 <31776152+djordjev7@users.noreply.github.com>
2017-11-20 11:14:08 -0500

    Merged UI

---
+++ app/src/main/java/com/example/vsergeychik390/hccmaps/StartPage.java
@@ -0,0 +1,23 @@
+package com.example.vsergeychik390.hccmaps;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class StartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+    }
+    public void open(View view) {
+        Intent intent = new Intent(this, Destination2.class);
+        startActivity(intent);
+        setContentView(R.layout.activity_destination2);
+    }
+}
