25596b6539120788a48e60f6ac272ce96f305cd1
Jacob Bashista <jacob@orange.haus>
2017-11-27 12:16:39 -0500

    Working Nav, Not Working Intent

--- app/src/main/java/com/example/vsergeychik390/hccmaps/Destination2.java
+++ app/src/main/java/com/example/vsergeychik390/hccmaps/Destination2.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Spinner;
+
+import java.util.List;
 
 public class Destination2 extends AppCompatActivity {
 
@@ -23,11 +26,11 @@ public class Destination2 extends AppCompatActivity {
             //Destination spinners
             Spinner destBuildingSpinner = (Spinner)findViewById(R.id.destBuildingSpinner);
             Spinner destFloorSpinner = (Spinner)findViewById(R.id.destFloorSpinner);
-            Spinner destRoomSpinner = (Spinner)findViewById(R.id.destRoomSpinner);
+            final Spinner destRoomSpinner = (Spinner)findViewById(R.id.destRoomSpinner);
             //Location spinner objects
             Spinner buildingSpinner = (Spinner)findViewById(R.id.buildingSpinner);
-            Spinner floorSpinner = (Spinner)findViewById(R.id.floorSpinner);
-            Spinner roomSpinner = (Spinner)findViewById(R.id.roomSpinner);
+            final Spinner floorSpinner = (Spinner)findViewById(R.id.floorSpinner);
+            final Spinner roomSpinner = (Spinner)findViewById(R.id.roomSpinner);
             //Message
             builder.setMessage("Location: " + buildingSpinner.getSelectedItem().toString() + " " +
                     floorSpinner.getSelectedItem().toString() + " Rm " + roomSpinner.getSelectedItem().toString()
@@ -37,6 +40,130 @@ public class Destination2 extends AppCompatActivity {
             //add the buttons
             builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener(){
                 public void onClick(DialogInterface dialog,int which){
+
+                    String sRoom = roomSpinner.getSelectedItem().toString();
+                    String eRoom = destRoomSpinner.getSelectedItem().toString();
+
+                    int[] startCord = new int[]{};
+                    int[] endCord = new int[]{};
+
+                    switch (sRoom){
+                        case "502":
+                            startCord = new int[]{28,3};
+                            break;
+                        case "503":
+                            startCord = new int[]{30,1};
+                            break;
+                        case "505":
+                            startCord = new int[]{28,1};
+                            break;
+                        case "507":
+                            startCord = new int[]{26,1};
+                            break;
+                        case "509":
+                            startCord = new int[]{24,1};
+                            break;
+                        case "513":
+                            startCord = new int[]{17,3};
+                            break;
+                        case "514":
+                            startCord = new int[]{13,3};
+                            break;
+                        case "516":
+                            startCord = new int[]{7,3};
+                            break;
+                        case "518":
+                            startCord = new int[]{3,3};
+                            break;
+                        default:
+                            break;
+                    }
+                    switch (eRoom){
+                        case "502":
+                            endCord = new int[]{28,3};
+                            break;
+                        case "503":
+                            endCord = new int[]{30,1};
+                            break;
+                        case "505":
+                            endCord = new int[]{28,1};
+                            break;
+                        case "507":
+                            endCord = new int[]{26,1};
+                            break;
+                        case "509":
+                            endCord = new int[]{24,1};
+                            break;
+                        case "513":
+                            endCord = new int[]{17,3};
+                            break;
+                        case "514":
+                            endCord = new int[]{13,3};
+                            break;
+                        case "516":
+                            endCord = new int[]{7,3};
+                            break;
+                        case "518":
+                            endCord = new int[]{3,3};
+                            break;
+                        default:
+                            break;
+                    }
+
+                    char[][] charMatrix = {
+                            {'@', '@', '@', '@', '@', '@', '@'},
+                            {'@', '@', '.', '.', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '.', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '.', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '.', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '.', '@'},
+                            {'@', '.', '.', '.', '.', '.', '@'},
+                            {'@', '@', '.', '@', '@', '.', '@'},
+                            {'@', '@', '.', '.', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '.', '.', '.', '.', '.', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '.', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '.', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '.', '.', '.', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '.', '.', '@', '@', '@', '@'},
+                            {'@', '@', '.', '@', '@', '@', '@'},
+                            {'@', '.', '.', '@', '@', '@', '@'},
+                            {'@', '@', '@', '@', '@', '@', '@'}
+                    };
+
+                    AStarNav nav = new AStarNav();
+
+                    List<String[]> route = nav.getPath(startCord, endCord, charMatrix);
+
+                    Intent directionInt = new Intent(Destination2.this, Directions.class);
+
+                    String routeString = "";
+
+                    for (int i = 0; i < route.size(); i++){
+                        routeString += route.get(i)[0] + " ";
+                        routeString += route.get(i)[1] + "\n";
+                    }
+
+                    Log.d("Route: ", routeString);
+
+                    directionInt.putExtra("directions", routeString);
+
                     startActivity(new Intent(Destination2.this, Directions.class));
                 }
             });
--- app/src/main/java/com/example/vsergeychik390/hccmaps/Directions.java
+++ app/src/main/java/com/example/vsergeychik390/hccmaps/Directions.java
@@ -1,7 +1,12 @@
 package com.example.vsergeychik390.hccmaps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Directions extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class Directions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directions);
+
+        Intent intent = getIntent();
+        String directions = intent.getStringExtra("directions");
+
+
+
+        TextView routeBox = (TextView) findViewById(R.id.messageBox);
+
+        routeBox.setText(directions);
+
+
     }
 }
